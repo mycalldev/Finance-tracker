a318713ca9920d099cc32e4c91730dd41b29096a
mycallCampbell <mycallcampbell@gmail.com>
2024-03-20 15:53:07 +0000

    setup html structure of app

---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,17 @@
+<template>
+    <h3>Add New Transaction</h3>
+    <form id="form">
+        <div class="form-control">
+            <label for="text">Text</label>
+            <input type="text" id="text" placeholder="Enter Text...." />
+        </div>
+
+        <div class="form-control">
+            <label for="amount">Amount <br />
+            (negative - expense, positive - income) 
+            </label>
+            <input type="number" id="amount" placeholder="Enter Amount...." />
+        </div>
+        <button class="btn">Add Transaction</button>
+    </form>
+</template>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,15 @@
+<template>
+    <h3>History</h3>
+    <ul id="list" class="list">
+        <li class="minus">
+        Cash <span>-£400</span>
+        <button class="delete-btn">x</button>
+    </li>
+    <li class="plus">
+        Payment <span>£800</span>
+        <button class="delete-btn">x</button>
+    </li>
+
+    </ul>
+    
+</template>
